8219685f0b205c4bcf71dc3d01c19bff32fabc04
rengzhengcodes <rzhengcontacts@gmail.com>
2020-10-28 18:45:04 -0400

    PointTester.java implemented for unit circle and randomized points.

--- PointTester.java
+++ PointTester.java
@@ -2,11 +2,55 @@ public class PointTester {
 
   public static void main(String[] args) {
     boolean failure = false;
+    failure = distanceTo();
     failure = equalTester();
   }
 
-  public static void errorHorn() {
-    System.out.println("ERROR\nERROR\nERROR\n");
+  public static boolean distanceTo() {
+    System.out.println("distanceTo() TEST");
+    boolean failure = false;
+    Point origin = new Point(0, 0);
+    Point[] randomizedPoints = new Point[100];
+    for (int index = 0; index < randomizedPoints.length; index++) {
+      double angle = (Math.random() * 2 * Math.PI);
+      randomizedPoints[index] = new Point(Math.cos(angle), Math.sin(angle));
+    }
+
+    System.out.println("RANDOMIZED UNIT CIRCLE TEST");
+    for (int index = 0; index < randomizedPoints.length; index++) {
+      if (Math.round(randomizedPoints[index].distanceTo(origin) * Math.pow(10, 15))/Math.pow(10, 15) == 1) {//we round here because FP errors can cause the last digit to be off a bit.
+        //System.out.println("TRUTH TEST PASSED AT RUN: " + index);             //if you want live printouts uncomment this.
+      } else {
+        failure = true;
+        System.out.println("WE HAVE FAILED");
+        System.out.println("EXPECTED: " + 1);
+        System.out.println("ACTUAL: "  + randomizedPoints[index].distanceTo(origin));
+      }
+    }
+
+    System.out.println("RANDOMIZED RANDOM TEST");
+    for (int index = 0; index < randomizedPoints.length; index++) {
+      randomizedPoints[index] = new Point(Math.random(), Math.random());
+    }
+    for (int index = 0; index < randomizedPoints.length; index++) {
+      double selfCalculatedDistance = Math.sqrt(Math.pow(randomizedPoints[index].getX(), 2) + Math.pow(randomizedPoints[index].getY(), 2));
+      if (randomizedPoints[index].distanceTo(origin) == selfCalculatedDistance) {//we round here because FP errors can cause the last digit to be off a bit.
+        //System.out.println("TRUTH TEST PASSED AT RUN: " + index);             //if you want live printouts uncomment this.
+      } else {
+        failure = true;
+        System.out.println("WE HAVE FAILED");
+        System.out.println("EXPECTED: " + selfCalculatedDistance);
+        System.out.println("ACTUAL: "  + randomizedPoints[index].distanceTo(origin));
+      }
+    }
+
+    if (failure) {
+      System.out.println("AT LEAST ONE TEST FAILED!!!!");
+    } else {
+      System.out.println("TESTS PASSED!");
+    }
+
+    return failure;
   }
 
   public static boolean equalTester() {
@@ -21,7 +65,7 @@ public class PointTester {
     for (int index = 0; index < randomizedPoints.length; index++) {
       Point reference = new Point(randomizedPoints[index]);
       if (randomizedPoints[index].equals(reference)) {
-        //System.out.println("TRUTH TEST PASSED AT RUN: " + index);             if you want live printouts uncomment this.
+        //System.out.println("TRUTH TEST PASSED AT RUN: " + index);             //if you want live printouts uncomment this.
       } else {
         failure = true;
         System.out.println("WE HAVE FAILED");
@@ -35,12 +79,33 @@ public class PointTester {
     for (int index = 0; index < randomizedPoints.length; index++) {
       Point reference = new Point(1, 1);
       if (!randomizedPoints[index].equals(reference)) {
-        //System.out.println("TRUTH TEST PASSED AT RUN: " + index);             if you want live printouts uncomment this.
+        //System.out.println("TRUTH TEST PASSED AT RUN: " + index);             //if you want live printouts uncomment this.
       } else {
         failure = true;
         System.out.println("WE HAVE FAILED");
         System.out.println("REFERENCE: " + reference.getX() + "\t" + reference.getY());
         System.out.println("ACTUAL: "  + randomizedPoints[index].getX() + "\t" + randomizedPoints[index].getY());
+      }
+    }
+
+    //static barrage
+    System.out.println("STATIC FIRE TEST");
+    Point[][] pointsPairs = {
+      {new Point(1, 1), new Point(1, 1)},
+      {new Point(1, 2), new Point(2, 1)},
+    };
+    boolean[] pointsPairsEquality = {
+      true,
+      false,
+    };
+    for (int index = 0; index < pointsPairs.length; index++) {
+      if (pointsPairs[index][0].equals(pointsPairs[index][1]) == pointsPairsEquality[index]) {
+        //System.out.println("TRUTH TEST PASSED AT RUN: " + index);             //if you want live printouts uncomment this.
+      } else {
+        failure = true;
+        System.out.println("WE HAVE FAILED");
+        System.out.println("POINT 0: " + pointsPairs[index][0].getX() + "\t" + pointsPairs[index][0].getY());
+        System.out.println("POINT 1: " + pointsPairs[index][1].getX() + "\t" + pointsPairs[index][1].getY());
       }
     }
 
