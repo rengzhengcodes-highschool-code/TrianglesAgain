5b7ad2f23f60382738b63160786650979a3f574b
rengzhengcodes <rzhengcontacts@gmail.com>
2020-10-29 15:52:23 -0400

    toString() tester added and passed.

--- TriangleTester.java
+++ TriangleTester.java
@@ -6,6 +6,7 @@ public class TriangleTester {
     failure = getPerimeterTester() || failure;
     failure = getAreaTester() || failure;
     failure = classifyTester() || failure;
+    failure = toStringTester() || failure;
 
     if (failure) {
       System.out.println("\nThere's a bug in your class");
@@ -130,7 +131,7 @@ public class TriangleTester {
   }
 
   public static boolean classifyTester() {
-    System.out.println("\n classify() TEST");
+    System.out.println("\nclassify() TEST");
     boolean failure = false;
     Triangle[] triangles = {
       new Triangle(0,0,0,0,0,0),                                                //degenerate equilateral
@@ -179,5 +180,55 @@ public class TriangleTester {
 
     return failure;
   }
+
+  public static boolean toStringTester() {
+    System.out.println("\ntoString() TEST");
+    boolean failure = false;
+    Triangle[] triangles = {
+      new Triangle(0,0,0,0,0,0),                                                //degenerate equilateral
+      new Triangle(12.21321,12321.232,12.21321,12321.232,12.21321,12321.232),   //degenerate FP equilateral
+      new Triangle(0,0,0,0,0,1),                                                //degenerate isosceles (up down)
+      new Triangle(1,0,0,0,0,0),                                                //degenerate isosceles (left right)
+      new Triangle(0,0,1,0,3,0),                                                //degenerate scalene
+      new Triangle(0,1,1,0,0,0),                                                //isosceles right triangle
+      new Triangle(1.023,12,123,1.194,234325,21321),                            //scalene
+      new Triangle(0,1,0,0,1,0.5),                                              //isosceles
+      new Triangle(0.1,0.1,-1.9,0.1,-0.9,-1.632051),                            //equilateral
+      new Triangle(-10,0,0,0,-5,2),                                             //isosceles
+      new Triangle(-1000,0,0,1,1000,0),                                         //isosceles
+    };
+    String[] expectedString = {
+      "v1(0.0, 0.0) v2(0.0, 0.0) v3(0.0, 0.0)",
+      "v1(12.21321, 12321.232) v2(12.21321, 12321.232) v3(12.21321, 12321.232)",
+      "v1(0.0, 0.0) v2(0.0, 0.0) v3(0.0, 1.0)",
+      "v1(1.0, 0.0) v2(0.0, 0.0) v3(0.0, 0.0)",
+      "v1(0.0, 0.0) v2(1.0, 0.0) v3(3.0, 0.0)",
+      "v1(0.0, 1.0) v2(1.0, 0.0) v3(0.0, 0.0)",
+      "v1(1.023, 12.0) v2(123.0, 1.194) v3(234325.0, 21321.0)",
+      "v1(0.0, 1.0) v2(0.0, 0.0) v3(1.0, 0.5)",
+      "v1(0.1, 0.1) v2(-1.9, 0.1) v3(-0.9, -1.632051)",
+      "v1(-10.0, 0.0) v2(0.0, 0.0) v3(-5.0, 2.0)",
+      "v1(-1000.0, 0.0) v2(0.0, 1.0) v3(1000.0, 0.0)",
+    };
+
+    for (int index = 0; index < triangles.length; index++) {
+      if (triangles[index].toString().equals(expectedString[index])) {          //The check is set up this way to counteract FP value errors from the difference in area finding methods.
+        //System.out.println("toString() TEST PASSED AT RUN: " + index);          //if you want live printouts uncomment this.
+      } else {
+        failure = true;
+        System.out.println("WE HAVE FAILED");
+        System.out.println("EXPECTED: " + expectedString[index]);
+        System.out.println("ACTUAL: "  + triangles[index].toString());
+      }
+    }
+
+    if (failure) {
+      System.out.println("toString() DOESN'T WORK");
+    } else {
+      System.out.println("toString() WORKS");
+    }
+
+     return failure;
+   }
 
 }
